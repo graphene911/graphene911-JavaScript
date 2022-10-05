34861828a4ecea8d606702343c015b3dfc0a9a5e
graphene911 <graphene911@gmail.com>
2022-10-05 20:43:03 +0900

    add

---
+++ JS 12. 반복문.js
@@ -0,0 +1,25 @@
+// 반복문
+
+// for문
+// for(let i = 0; i < 10; i++){
+//   console.log(i);
+// }
+
+// // while 문
+
+// let i = 0;
+// while(i < 10){
+//   console.log(i);
+//   i++;
+// }
+
+// do.. while 문
+
+let i = 0;
+
+do {
+  i++;
+  console.log(i);
+} while( i < 10 ){
+  
+}
